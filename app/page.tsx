fa2da0b09bee5be125cc950680a2aec297c1957d
bertjee321 <b.kruiter.91@gmail.com>
2026-01-14 21:08:25 +0100

    Remove unused import of DesignTabs component from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import DesignConfigForm from "@/components/design/DesignConfigForm";
-import DesignTabs from "@/components/design/DesignTabs";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import MainContent from "@/components/layout/MainContent";
